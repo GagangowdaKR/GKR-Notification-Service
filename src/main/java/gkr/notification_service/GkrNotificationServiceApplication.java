2221996b35bf67236c2d2412efd083eb12ba2efa
GagangowdaKR <gagandarshan22@gmail.com>
2026-08-03 21:45:28 +0530

    Microservices Template For NotificationService

---
+++ src/main/java/gkr/notification_service/GkrNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package gkr.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkrNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkrNotificationServiceApplication.class, args);
+	}
+
+}
